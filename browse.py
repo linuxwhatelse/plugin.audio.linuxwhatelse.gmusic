b61d1992efb3cb824b31bfee181dba16f1fe94a7
Tadly <jan@tadly.de>
2015-11-06 18:39:24 +0100

    added situations to listen now section

--- browse.py
+++ browse.py
@@ -55,6 +55,8 @@ def main_menu():
 ########################
 @mapper.url('^/browse/listen-now/$')
 def listen_now():
+    gmusic.login()
+
     items = [
         # URL , ListItem, isFolder
         ( mapper.build_url(url=url, paths=['play', 'station'], queries={'station_id': 'IFL'}, overwrite_path=True, \
@@ -65,6 +67,15 @@ def listen_now():
 
     ]
 
+    # Get current situation and add it to the list
+    situations = gmusic.get_situations(_locale_code)
+
+    # We save the current response so we don't have to fetch it again when the users selects it
+    with open(os.path.join(_cache_dir, 'situations.json'), 'w+') as f:
+        f.write(json.dumps(situations, indent=2))
+
+    items.insert(1, ( mapper.build_url(url, ['situations']), xbmcgui.ListItem(label=situations['primaryHeader'], iconImage=resources.IMG_ALBUM, thumbnailImage=resources.IMG_ALBUM),    True ))
+
     for item in items:
         item[1].addContextMenuItems([],True)
 
@@ -74,6 +85,61 @@ def listen_now():
     )
 
     list_items(items)
+
+@mapper.url('^/browse/listen-now/situations/$')
+def listen_now_situations():
+    situations = None
+    with open(os.path.join(_cache_dir, 'situations.json'), 'r') as f:
+        situations = json.loads(f.read())
+
+    if situations:
+        items = build_situation_listitems(situations['situations'])
+        list_situations(items)
+
+@mapper.url('^/browse/listen-now/situation/$')
+def listen_now_situation(situation_id):
+    situations = None
+    with open(os.path.join(_cache_dir, 'situations.json'), 'r') as f:
+        situations = json.loads(f.read())
+
+    if not situations:
+        list_items([])
+
+    for situation in situations['situations']:
+        if situation_id != situation['id']:
+            # In some cases, a situation can have situations as childs (not stations),
+            # therefore we have to check if one of the sub-situations matches our id
+            if 'situations' in situation:
+                for situation in situation['situations']:
+                    if situation_id != situation['id']:
+                        continue
+            else:
+                continue
+
+        if 'situations' in situation:
+            list_situations(build_situation_listitems(situation['situations']))
+
+        elif 'stations' in situation:
+            stations = situation['stations']
+            new_stations = []
+            for station in stations:
+                art = ''
+                for img_urls in station['compositeArtRefs']:
+                    if int(img_urls['aspectRatio']) == 1:
+                        art = img_urls['url']
+                        break
+
+                tmp_station = {
+                    'name': station['name'],
+                    'imageUrls': [
+                        {'url': art}
+                    ],
+                    'curatedStationId': station['seed']['curatedStationId'],
+                    'description': station['description']
+                }
+
+                new_stations.append(tmp_station)
+            list_stations(build_station_listitems(new_stations))
 
 @mapper.url('^/browse/listen-now/albums/$')
 def listen_now_albums():
@@ -765,7 +831,10 @@ def build_station_listitems(stations):
         )
 
         item.setInfo('music', {
-            'album': station_name
+            'album': station_name,
+            # This might look a little bit wrong, but as long as no one complains about it,
+            # we'll leave that in so we have that nice description at least somewhere
+            'artist': station['description'] if 'description' in station else '',
         })
 
         query = {}
@@ -809,6 +878,37 @@ def build_station_listitems(stations):
 def list_stations(listitems, allow_view_overwrite=True):
     xbmcplugin.setContent(addon_handle, 'albums')
     list_items(listitems, allow_view_overwrite, int(_addon.getSetting('view_id_stations')), False)
+
+def build_situation_listitems(situations):
+    items = []
+    for situation in situations:
+        situation_id    = situation['id']
+        situation_title = situation['title']
+        situation_art   = situation['imageUrl'] if 'imageUrl' in situation else resources.IMG_ALBUM
+
+        item = xbmcgui.ListItem(
+            label           = situation_title,
+            iconImage       = situation_art,
+            thumbnailImage  = situation_art,
+        )
+
+        item.setInfo('music', {
+            'album'   : situation_title,
+            # This might look a little bit wrong, but as long as no one complains about it,
+            # we'll leave that in so we have that nice description at least somewhere
+            'artist'  : situation['description']  if 'description' in situation else '',
+        })
+
+        item.addContextMenuItems(items=[], replaceItems=True)
+
+        items.append(
+            (mapper.build_url(url=url, paths=['browse', 'listen-now', 'situation'], \
+                queries={'situation_id': situation_id}, overwrite_path=True, overwrite_query=True), item, True)
+            )
+
+    return items
+def list_situations(listitems, allow_view_overwrite=True):
+    list_albums(listitems, allow_view_overwrite)
 
 def build_song_listitems(tracks, station_id=None, my_library=False, my_library_playlist=False):
     items = []
