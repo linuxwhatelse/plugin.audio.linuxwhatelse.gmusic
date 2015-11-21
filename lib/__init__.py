6b00f6a28f75f783761ef9fe978a15313e03be00
Tadly <jan@tadly.de>
2015-11-21 11:56:40 +0100

    preparation for osx and openelec support

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,15 +1,31 @@
-from os import pardir
-from os.path import dirname, abspath, join
-from site import addsitedir
+import os
+import site
 from sys import platform as _platform
 
-lib_dir       = dirname(abspath(__file__))
-lib_win32_dir = join(lib_dir, pardir, 'lib-win32')
-lib_unix_dir  = join(lib_dir, pardir, 'lib-unix')
+# Define all lib available lib directories
+lib_dir        = os.path.dirname(os.path.abspath(__file__))      # Default cross plattform libraries
+lib_sys_dir    = os.path.join(lib_dir, os.pardir, 'lib-sys')     # Libraries normally included with python but missing on some plattforms
+lib_unix_dir   = os.path.join(lib_dir, os.pardir, 'lib-unix')    # Libraries built for unix plattforms
+lib_darwin_dir = os.path.join(lib_dir, os.pardir, 'lib-darwin')  # Libraries built for darwin plattforms
+lib_win32_dir  = os.path.join(lib_dir, os.pardir, 'lib-win32')   # Libraries built for win plattforms
 
-if _platform == 'win32':
-    addsitedir( lib_win32_dir )
-else:
-    addsitedir( lib_unix_dir )
 
-addsitedir( lib_dir )
+# Add all cross plattform dependencies to the python path
+site.addsitedir( lib_dir )
+
+# Add all plattform dependent dependencies to the python path
+if _platform == 'linux' or _platform == 'linux2':
+    site.addsitedir( lib_unix_dir )
+
+elif _platform == 'darwin':
+    site.addsitedir( lib_win32_dir )
+
+elif _platform == 'win32':
+    site.addsitedir( lib_win32_dir )
+
+
+# Add dependencies if missing on a plattform (e.g. OpenELEC)
+try:
+    import unittest
+except ImportError:
+    site.addsitedir( os.path.join(lib_sys_dir, 'unittest') )
