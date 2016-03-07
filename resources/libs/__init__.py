62c0df4c0fbb10357c28fbf361cf639e37911fbc
Tadly <jan@tadly.de>
2016-03-07 16:46:50 +0100

    removed darwin libs as I don't expect it to be supported anytime soon

--- resources/libs/__init__.py
+++ resources/libs/__init__.py
@@ -2,12 +2,11 @@ import os
 import site
 from sys import platform as _platform
 
-# Define all lib available lib directories
+# Define all available lib directories
 lib_root_dir   = os.path.dirname(os.path.abspath(__file__))
 lib_dir        = os.path.join(lib_root_dir, 'lib')         # Default cross plattform libraries
 lib_sys_dir    = os.path.join(lib_root_dir, 'lib-sys')     # Libraries normally included with python but missing on some plattforms
 lib_unix_dir   = os.path.join(lib_root_dir, 'lib-unix')    # Libraries built for unix plattforms
-lib_darwin_dir = os.path.join(lib_root_dir, 'lib-darwin')  # Libraries built for darwin plattforms
 lib_win32_dir  = os.path.join(lib_root_dir, 'lib-win32')   # Libraries built for win plattforms
 
 # Add all cross plattform dependencies to the python path
@@ -16,9 +15,6 @@ site.addsitedir( lib_dir )
 # Add all plattform dependent dependencies to the python path
 if _platform == 'linux' or _platform == 'linux2':
     site.addsitedir( lib_unix_dir )
-
-elif _platform == 'darwin':
-    site.addsitedir( lib_darwin_dir )
 
 elif _platform == 'win32':
     site.addsitedir( lib_win32_dir )
