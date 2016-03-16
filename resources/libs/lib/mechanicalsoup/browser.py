12388e712baa398a780d8ed9098f11ca4422632e
Tadly <jan@tadly.de>
2016-03-16 09:25:40 +0100

    bumped mechanicalsoup to 0.4.0

--- resources/libs/lib/mechanicalsoup/browser.py
+++ resources/libs/lib/mechanicalsoup/browser.py
@@ -2,91 +2,111 @@ import requests
 import bs4
 from six.moves import urllib
 from six import string_types
+from .form import Form
 
-class Browser:
-    def __init__(self, session=None):
+
+class Browser(object):
+
+    def __init__(self, session=None, soup_config=None):
         self.session = session or requests.Session()
+        self.soup_config = soup_config or dict()
 
     @staticmethod
-    def add_soup(response):
-        if 'text/html' in response.headers.get('Content-Type', ''):
-            try:
-                response.soup = bs4.BeautifulSoup(response.content)
-            except:
-                pass
+    def add_soup(response, soup_config):
+        if "text/html" in response.headers.get("Content-Type", ""):
+            response.soup = bs4.BeautifulSoup(
+                response.content, **soup_config)
 
     def request(self, *args, **kwargs):
         response = self.session.request(*args, **kwargs)
-        Browser.add_soup(response)
+        Browser.add_soup(response, self.soup_config)
         return response
 
     def get(self, *args, **kwargs):
         response = self.session.get(*args, **kwargs)
-        Browser.add_soup(response)
+        Browser.add_soup(response, self.soup_config)
         return response
 
     def post(self, *args, **kwargs):
         response = self.session.post(*args, **kwargs)
-        Browser.add_soup(response)
+        Browser.add_soup(response, self.soup_config)
         return response
 
     def _build_request(self, form, url=None, **kwargs):
-        method = form['method']
-        action = form['action']
+        method = form.get("method", "get")
+        action = form.get("action")
         url = urllib.parse.urljoin(url, action)
+        if url is None:  # This happens when both `action` and `url` are None.
+            raise ValueError('no URL to submit to')
 
         # read http://www.w3.org/TR/html5/forms.html
-        data = kwargs.get('data') or dict()
-        files = kwargs.get('files') or dict()
-        
+        data = kwargs.pop("data", dict())
+        files = kwargs.pop("files", dict())
+
         for input in form.select("input"):
-            name = input.get('name')
-            value = input.get('value', '') # web browsers use empty string for inputs with missing values
+            name = input.get("name")
             if not name:
                 continue
 
-            if input.get('type') in ('radio', 'checkbox') and 'checked' not in input.attrs:
-                continue
+            if input.get("type") in ("radio", "checkbox"):
+                if "checked" not in input.attrs:
+                    continue
+                value = input.get("value", "on")
+            else:
+                # web browsers use empty string for inputs with missing values
+                value = input.get("value", "")
 
-            if input.get('type') == 'checkbox':
-                if not name in data:
-                    data[name] = list()
-                data[name].append(value)
-            
-            elif input.get('type') == 'file':
+            if input.get("type") == "checkbox":
+                data.setdefault(name, []).append(value)
+
+            elif input.get("type") == "file":
                 # read http://www.cs.tut.fi/~jkorpela/forms/file.html
-                # in web browsers, file upload only happens if the form's (or submit button's) enctype attribute is set to 'multipart/form-data'. we don't care, simplify.
+                # in web browsers, file upload only happens if the form"s (or
+                # submit button"s) enctype attribute is set to
+                # "multipart/form-data". we don"t care, simplify.
                 if not value:
                     continue
                 if isinstance(value, string_types):
-                    value = open(value, 'rb')
+                    value = open(value, "rb")
                 files[name] = value
-                
+
             else:
                 data[name] = value
-            
+
         for textarea in form.select("textarea"):
-            name = textarea.get('name')
+            name = textarea.get("name")
             if not name:
                 continue
             data[name] = textarea.text
 
         for select in form.select("select"):
-            name = select.get('name')
+            name = select.get("name")
             if not name:
                 continue
+            multiple = "multiple" in select.attrs
+            values = []
             for i, option in enumerate(select.select("option")):
-                if i == 0 or "selected" in option.attrs:
-                    data[name] = option.get('value', '')
- 
-        return requests.Request(method, url, data=data, files=files, **kwargs)
+                if (i == 0 and not multiple) or "selected" in option.attrs:
+                    values.append(option.get("value", ""))
+            if multiple:
+                data[name] = values
+            elif values:
+                data[name] = values[-1]
+
+        if method.lower() == "get":
+            kwargs["params"] = data
+        else:
+            kwargs["data"] = data
+        return requests.Request(method, url, files=files, **kwargs)
 
     def _prepare_request(self, form, url=None, **kwargs):
         request = self._build_request(form, url, **kwargs)
         return self.session.prepare_request(request)
 
     def submit(self, form, url=None, **kwargs):
+        if isinstance(form, Form):
+            form = form.form
         request = self._prepare_request(form, url, **kwargs)
         response = self.session.send(request)
-        Browser.add_soup(response)
+        Browser.add_soup(response, self.soup_config)
         return response
